2f2fc70e78dda6231d334c6553452014fda50cbb
sam <sam@qborg.com>
2017-12-21 19:05:57 +0200

    added button for table result

--- build/bundle.js
+++ build/bundle.js
@@ -43215,6 +43215,20 @@
 	                        )
 	                    ),
 	                    _react2['default'].createElement(
+	                        'div',
+	                        { className: 'buttonGroup' },
+	                        _react2['default'].createElement(
+	                            'button',
+	                            null,
+	                            'Верно'
+	                        ),
+	                        _react2['default'].createElement(
+	                            'button',
+	                            null,
+	                            'Неверно'
+	                        )
+	                    ),
+	                    _react2['default'].createElement(
 	                        'h1',
 	                        null,
 	                        ' Скорее всего , Вам должно быть предложено молекулярное исследование тканей для точного определения возможных вариантов новейших линий лечения .'
@@ -43279,7 +43293,7 @@
 
 
 	// module
-	exports.push([module.id, ".test {\n  -moz-transform: skew(25deg, 10deg);\n  -o-transform: skew(25deg, 10deg);\n  -ms-transform: skew(25deg, 10deg);\n  -webkit-transform: skew(25deg, 10deg);\n  transform: skew(25deg, 10deg);\n  moz-transform-origin: top left;\n  -o-transform-origin: top left;\n  -ms-transform-origin: top left;\n  -webkit-transform-origin: top left;\n  transform-origin: top left;\n  position: absolute;\n  top: 25%;\n  bottom: 25%;\n  left: 25%;\n  right: 25%;\n  background-color: rgba(20, 20, 20, 0.5); }\n\n.smarttestresult {\n  width: 100%;\n  min-height: 100%;\n  background-color: white; }\n  .smarttestresult .bootomLogic {\n    margin: 0 auto;\n    width: 80%;\n    margin-top: 20px;\n    margin-bottom: 20px;\n    min-height: 100%;\n    background-color: white;\n    font-size: 16px;\n    padding: 10px;\n    text-align: center; }\n    .smarttestresult .bootomLogic h1 {\n      color: #2f8f9b;\n      font-family: \"MuseoSans-Light\"; }\n      @media (max-width: 480px) {\n        .smarttestresult .bootomLogic h1 {\n          font-size: 17px; } }\n    .smarttestresult .bootomLogic h2 {\n      font-family: \"MuseoSans-Light\";\n      color: #54aab1; }\n      @media (max-width: 480px) {\n        .smarttestresult .bootomLogic h2 {\n          font-size: 14px; } }\n    .smarttestresult .bootomLogic table {\n      margin-top: 20px;\n      margin-bottom: 50px;\n      table-layout: fixed;\n      width: 100%;\n      border-collapse: collapse;\n      border: 1px solid black;\n      color: white; }\n      .smarttestresult .bootomLogic table td {\n        padding: 10px; }\n      .smarttestresult .bootomLogic table tr {\n        word-break: break-all;\n        height: 30px; }\n      .smarttestresult .bootomLogic table tr:nth-child(odd) {\n        background-color: #54aab1; }\n      .smarttestresult .bootomLogic table tr:nth-child(even) {\n        background-color: #2f8f9b; }\n", ""]);
+	exports.push([module.id, ".test {\n  -moz-transform: skew(25deg, 10deg);\n  -o-transform: skew(25deg, 10deg);\n  -ms-transform: skew(25deg, 10deg);\n  -webkit-transform: skew(25deg, 10deg);\n  transform: skew(25deg, 10deg);\n  moz-transform-origin: top left;\n  -o-transform-origin: top left;\n  -ms-transform-origin: top left;\n  -webkit-transform-origin: top left;\n  transform-origin: top left;\n  position: absolute;\n  top: 25%;\n  bottom: 25%;\n  left: 25%;\n  right: 25%;\n  background-color: rgba(20, 20, 20, 0.5); }\n\n.smarttestresult {\n  width: 100%;\n  min-height: 100%;\n  background-color: white; }\n  .smarttestresult .bootomLogic {\n    margin: 0 auto;\n    width: 80%;\n    margin-top: 20px;\n    margin-bottom: 20px;\n    min-height: 100%;\n    background-color: white;\n    font-size: 16px;\n    padding: 10px;\n    text-align: center; }\n    .smarttestresult .bootomLogic h1 {\n      color: #2f8f9b;\n      font-family: \"MuseoSans-Light\"; }\n      @media (max-width: 480px) {\n        .smarttestresult .bootomLogic h1 {\n          font-size: 17px; } }\n    .smarttestresult .bootomLogic h2 {\n      font-family: \"MuseoSans-Light\";\n      color: #54aab1; }\n      @media (max-width: 480px) {\n        .smarttestresult .bootomLogic h2 {\n          font-size: 14px; } }\n    .smarttestresult .bootomLogic table {\n      margin-top: 20px;\n      margin-bottom: 50px;\n      table-layout: fixed;\n      width: 100%;\n      border-collapse: collapse;\n      border: 1px solid black;\n      color: white; }\n      .smarttestresult .bootomLogic table td {\n        padding: 10px; }\n      .smarttestresult .bootomLogic table tr {\n        word-break: break-all;\n        height: 30px; }\n      .smarttestresult .bootomLogic table tr:nth-child(odd) {\n        background-color: #54aab1; }\n      .smarttestresult .bootomLogic table tr:nth-child(even) {\n        background-color: #2f8f9b; }\n    .smarttestresult .bootomLogic .buttonGroup {\n      display: -webkit-box;\n      display: -webkit-flex;\n      display: -moz-flex;\n      display: -ms-flexbox;\n      display: flex;\n      -ms-flex-pack: distribute;\n      -webkit-justify-content: space-around;\n      -moz-justify-content: space-around;\n      justify-content: space-around;\n      margin-bottom: 100px; }\n      .smarttestresult .bootomLogic .buttonGroup button {\n        cursor: pointer;\n        width: 120px;\n        height: 40px;\n        border: 1px solid #2f8f9b;\n        color: #2f8f9b; }\n        .smarttestresult .bootomLogic .buttonGroup button:hover {\n          background-color: #2f8f9b;\n          color: white;\n          transition: all .3s; }\n", ""]);
 
 	// exports
 
